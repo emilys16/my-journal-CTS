0aefe9d42aba5e46f6dcded1769fbcc1ca88a4c0
emilys16 <emmy.scott@hotmail.com>
2025-10-27 16:15:41 -0400

    sixth commit
    changed the theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ SOCIAL = (
 DEFAULT_PAGINATION = 10
 
 # COPY AND PASTED STUFF
-THEME = 'themes/pelican-hyde-master'
+THEME = 'themes/Flex'
 PROFILE_IMAGE = "DrawnProfile.png" 
 BIO = "A journal about Capybara Life."
 
